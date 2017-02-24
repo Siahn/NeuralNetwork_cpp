a63534ccf123eb7f04ebc16e9203b91758c6f0fd
Ishan Tiwathia <ti.siahn@gmail.com>
2017-02-24 14:50:23 -0800

    Added MNIST Examples & Implemented MNIST Read

--- NeuralNetwork/NeuralNetwork/NeuralNetwork.cpp
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork.cpp
@@ -15,6 +15,7 @@
 #include <fstream>
 #include <sstream>
 #include <ctime>
+#include "NeuralNetwork.h"
 using namespace std;
 
 
@@ -394,11 +395,56 @@ void display_vector(string label , vector <double> &v)
 	cout << endl;
 }
 
+int ReverseInt(int i)
+{
+	unsigned char ch1, ch2, ch3, ch4;
+	ch1 = i & 255;
+	ch2 = (i >> 8) & 255;
+	ch3 = (i >> 16) & 255;
+	ch4 = (i >> 24) & 255;
+	return((int)ch1 << 24) + ((int)ch2 << 16) + ((int)ch3 << 8) + ch4;
+}
+void ReadMNIST(int NumberOfImages, int DataOfAnImage, vector<vector<double>> &arr)
+{
+	arr.resize(NumberOfImages, vector<double>(DataOfAnImage));
+	ifstream file("C:\\Users\\tiwathia\\Documents\\GitHub\\NeuralNetwork_cpp\\Training Data\\MNIST\\t10k-images.idx3-ubyte", ios::binary);
+	if (file.is_open())
+	{
+		int magic_number = 0;
+		int number_of_images = 0;
+		int n_rows = 0;
+		int n_cols = 0;
+		file.read((char*)&magic_number, sizeof(magic_number));
+		magic_number = ReverseInt(magic_number);
+		file.read((char*)&number_of_images, sizeof(number_of_images));
+		number_of_images = ReverseInt(number_of_images);
+		file.read((char*)&n_rows, sizeof(n_rows));
+		n_rows = ReverseInt(n_rows);
+		file.read((char*)&n_cols, sizeof(n_cols));
+		n_cols = ReverseInt(n_cols);
+		for (int i = 0; i<number_of_images; ++i)
+		{
+			for (int r = 0; r<n_rows; ++r)
+			{
+				for (int c = 0; c<n_cols; ++c)
+				{
+					unsigned char temp = 0;
+					file.read((char*)&temp, sizeof(temp));
+					arr[i][(n_rows*r) + c] = (double)temp;
+					cout << arr[i][(n_rows*r) + c];
+				}
+				cout << endl;
+			}
+			cin.get();
+		}
+	}
+}
+
 
 int main()
 {
 
-	TrainingData training_data("C:\\Users\\tiwathia\\Documents\\GitHub\\NeuralNetwork_cpp\\Training Data\\XOR_training_250.txt");
+	TrainingData training_data("C:\\Users\\tiwathia\\Documents\\GitHub\\NeuralNetwork_cpp\\Training Data\\XOR\\XOR_training_250.txt");
 	vector <unsigned> network_topology;
 	training_data.get_topology(network_topology);
 	// e.g [3,2,1] creates 3 input neurons
@@ -454,7 +500,13 @@ int main()
 	double duration = (std::clock() - timer) / (double)CLOCKS_PER_SEC;
 	cout << endl << "      TRAINED IN : " << duration << " secs" << endl;
 	cout << endl << endl << "#######################################" << endl << endl;
+
 	cin.get();
+	
+	
+	vector<vector<double>> ar;
+	ReadMNIST(10000, 784, ar);
+	
 	return 0;
 }
 
